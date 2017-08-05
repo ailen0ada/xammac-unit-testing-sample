9a458b6f353ddf804d21266e659190eff8a91066
Tsubasa HIRANO <hirano@lifebear.co.jp>
2017-08-05 12:39:45 +0900

    add unit tests

---
+++ UnitTestingSample.Tests.NUnit/GeneratorTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using UnitTestingSample.Models;
+using NUnit.Framework;
+
+namespace UnitTestingSample.Tests
+{
+    [TestFixture]
+    public class GeneratorTests
+    {
+        [Test]
+        public void GeneratingTest()
+        {
+            var expectedPattern = @"^[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{6}_[0-9a-z]{7}$";
+
+            var generator = new TempFileNameGenerator();
+            var actual = generator.Generate();
+
+            Assert.True(Regex.IsMatch(actual, expectedPattern));
+        }
+    }
+}
---
+++ UnitTestingSample.Tests.Xunit/GeneratorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Xunit;
+using UnitTestingSample.Models;
+
+namespace UnitTestingSample.Tests
+{
+    public class GeneratorTests
+    {
+        [Fact]
+        public void GeneratingTest()
+        {
+            var expectedPattern = @"^[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{6}_[0-9a-z]{7}$";
+
+            var generator = new TempFileNameGenerator();
+            var actual = generator.Generate();
+
+            Assert.True(Regex.IsMatch(actual, expectedPattern));
+        }
+    }
+}
